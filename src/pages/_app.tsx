f06dff738a75a36a434f7409885d0655d7c6fe94
yonraz <yonatan.raz@frontegg.com>
2024-11-10 16:13:57 +0200

    second commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
+import { withFronteggApp } from "@frontegg/nextjs/pages";
+import "../styles/globals.css";
+function CustomApp({ Component, pageProps }: any) {
   return <Component {...pageProps} />;
 }
+
+export default withFronteggApp(CustomApp, {
+  hostedLoginBox: false,
+  authOptions: {
+    // keepSessionAlive: true, // Uncomment this in order to maintain the session alive
+  },
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,64 @@
-import Image from "next/image";
-import localFont from "next/font/local";
+"use client";
+import { useCallback, useState } from "react";
+import {
+  useAuth,
+  AdminPortal,
+  useAuthActions,
+  useTenantsState,
+  useTenantsActions,
+} from "@frontegg/nextjs";
+import { useRouter } from "next/router";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+export default function MyPage() {
+  const { user, isAuthenticated } = useAuth();
+  const { tenants, activeTenant } = useTenantsState();
+  const [selectedTenant, setSelectedTenant] = useState(activeTenant);
+  const { switchTenant } = useTenantsActions();
 
-export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const router = useRouter();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const logout = useCallback(() => {
+    router.replace("/account/logout");
+  }, [router]);
+
+  const handleTenantSwitch = () => {
+    if (selectedTenant && selectedTenant?.tenantId === activeTenant?.tenantId)
+      return;
+    switchTenant({ tenantId: selectedTenant!.tenantId });
+  };
+
+  return isAuthenticated ? (
+    <div>
+      <h1 className="font-extrabold text-3xl">My Page</h1>
+      <img src={user?.profilePictureUrl ?? ""} alt="user pfp" />
+      <div>
+        <span>Logged in as: {user?.name}</span>
+      </div>
+      <div>
+        <span>Active Tenant: {activeTenant?.name}</span>
+      </div>
+      <div>
+        <select
+          className=" p-1 rounded-sm mr-1  border-slate-200 border-2"
+          value={selectedTenant?.tenantId}
+          onChange={(e) =>
+            setSelectedTenant(
+              (prev) =>
+                tenants.find((t) => t.tenantId === e.target.value) ?? prev
+            )
+          }
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          {tenants.map((tenant) => (
+            <option value={tenant.tenantId}>{tenant.name}</option>
+          ))}
+        </select>
+        <button onClick={handleTenantSwitch}>Switch Tenant</button>
+      </div>
+      <div>
+        <button onClick={logout}>Log out</button>
+      </div>
     </div>
+  ) : (
+    <button onClick={() => router.push("/account/login")}>Log in</button>
   );
 }
